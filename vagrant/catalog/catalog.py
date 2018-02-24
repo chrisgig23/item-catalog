aeac3012c172dc5aa126dfa9965e471801d3c788
Christopher Giglio <cgiglio23@gmail.com>
2018-02-24 17:23:54 -0500

    Added home page route

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -3,10 +3,21 @@ app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
+from database_setup import Base, Category, CategoryItem
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = create_engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+@app.route('/')
+@app.route('/catalog/')
+def showCatalog():
+    categories = session.query(Category).all()
+    return render_template('catalog.html', categories = categories)
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
