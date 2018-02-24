54a9b13cabda46a38dfe13a75176b4d7d3f7f027
Christopher Giglio <cgiglio23@gmail.com>
2018-02-24 16:21:56 -0500

    Added catalog.py file with starter code for project

---
+++ vagrant/catalog/catalog.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = create_engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
