fdb6c207b54dde0baabe6831de9c621f00e150c0
Christopher Giglio <cgiglio23@gmail.com>
2018-03-05 20:02:14 -0500

    Add code to Add Edit & Delete items from category

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -17,11 +17,50 @@ def showCatalog():
     categories = session.query(Category).all()
     return render_template('catalog.html', categories = categories)
 
-@app.route('/catalog/<int: category_id>')
+@app.route('/catalog/<int:category_id>')
 def showCategory(category_id):
     category = session.query(Category).filter_by(id = category_id).one()
-    categoryItems = session.query(CategoryItems).filter_by(category_id = category.id)
+    categoryItems = session.query(CategoryItem).filter_by(category_id = category.id)
+    return render_template('category.html', category = category, items = categoryItems)
 
+@app.route('/catalog/<int:category_id>/new', methods=['GET', 'POST'])
+def newItem(category_id):
+    if request.method == 'POST':
+        newItem = CategoryItem(name = request.form['title'], description = request.form['description'], category_id = category_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('showCategory', category_id = category_id))
+    else:
+        listOfCategories = session.query(Category).all()
+        return render_template('newItem.html', category_id = category_id, listOfCategories = listOfCategories)
+
+@app.route('/catalog/<int:category_id>/<int:item_id>/edit', methods=['GET', 'POST'])
+def editItem(category_id, item_id):
+    editedItem = session.query(CategoryItem).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        if request.form['description']:
+            editedItem.description = request.form['description']
+        # if request.form['course']:
+        #     editedItem.course = request.form['course']
+
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('showCategory', category_id = category_id))
+    else:
+        listOfCategories = session.query(Category).all()
+        return render_template('editItem.html', category_id=category_id, item_id=item_id, item = editedItem, listOfCategories = listOfCategories)
+
+@app.route('/catalog/<int:category_id>/<int:item_id>/delete', methods=['GET', 'POST'])
+def deleteItem(category_id, item_id):
+    deletedItem = session.query(CategoryItem).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        session.delete(deletedItem)
+        session.commit()
+        return redirect(url_for('showCategory', category_id = category_id))
+    else:
+        return render_template('deleteItem.html', category_id=category_id, item_id=item_id, item=deletedItem)
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
