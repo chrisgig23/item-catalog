4946514cabc4eb62037aab6b3da9b5d79a445f29
Christopher Giglio <cgiglio23@gmail.com>
2018-03-30 11:22:34 -0400

    Added Google log out functionality

--- catalog.py
+++ catalog.py
@@ -117,6 +117,37 @@ def gconnect():
     return output
 
 
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
 #JSON APIs to view sporting goods information
 
 
@@ -144,6 +175,9 @@ def showItem(category_id, item_id):
 
 @app.route('/catalog/<int:category_id>/new', methods=['GET', 'POST'])
 def newItem(category_id):
+    if 'username' not in login_session:
+        # alert("You must be logged in to do that.")
+        return redirect('/login')
     if request.method == 'POST':
         newItem = CategoryItem(name = request.form['title'], description = request.form['description'], category_id = category_id)
         session.add(newItem)
